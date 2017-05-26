f50083ec4ebc762bd15ada711b1357685198f83b
shunsuke-sato <shunsuke.sato26@gmail.com>
2017-05-26 21:44:32 +0200

    Optimized hpsi and Laplacian in gceed.

--- GCEED/modules/laplacian2.f90
+++ GCEED/modules/laplacian2.f90
@@ -1,5 +1,5 @@
 ! Copyright 2017 Katsuyuki Nobusada, Masashi Noda, Kazuya Ishimura, Kenji Iida, Maiku Yamaguchi
-!
+!                Shunsuke A. Sato
 ! Licensed under the Apache License, Version 2.0 (the "License");
 ! you may not use this file except in compliance with the License.
 ! You may obtain a copy of the License at
@@ -33,11 +33,15 @@ implicit none
 integer :: ix,iy,iz,ist
 real(8) :: wk2(iwk2sta(1):iwk2end(1),iwk2sta(2):iwk2end(2),iwk2sta(3):iwk2end(3))
 real(8) :: lap_wk(iwk3sta(1):iwk3end(1),iwk3sta(2):iwk3end(2),iwk3sta(3):iwk3end(3))
-real(8) :: f0
+real(8) :: f0, Hgs_i2(3)
 
 f0=(1.d0/Hgs(1)**2   &
    +1.d0/Hgs(2)**2   &
    +1.d0/Hgs(3)**2)
+
+Hgs_i2(1) = 1d0/Hgs(1)**2
+Hgs_i2(2) = 1d0/Hgs(2)**2
+Hgs_i2(3) = 1d0/Hgs(3)**2
 
 !$OMP parallel do private(ist)
 do iz=iwk3sta(3),iwk3end(3)
@@ -46,9 +50,9 @@ do ix=iwk3sta(1),iwk3end(1)
   lap_wk(ix,iy,iz)=cNmat(0,Nd)*f0*wk2(ix,iy,iz)
   do ist=1,Nd
     lap_wk(ix,iy,iz)=lap_wk(ix,iy,iz)     &
-           +cNmat(ist,Nd)*( wk2(ix+ist,iy,iz)/Hgs(1)**2 + wk2(ix-ist,iy,iz)/Hgs(1)**2       &
-                           +wk2(ix,iy+ist,iz)/Hgs(2)**2 + wk2(ix,iy-ist,iz)/Hgs(2)**2       &
-                           +wk2(ix,iy,iz+ist)/Hgs(3)**2 + wk2(ix,iy,iz-ist)/Hgs(3)**2  )
+           +cNmat(ist,Nd)*( Hgs_i2(1)*(wk2(ix+ist,iy,iz)+wk2(ix-ist,iy,iz))  &
+                           +Hgs_i2(2)*(wk2(ix,iy+ist,iz)+wk2(ix,iy-ist,iz))  &
+                           +Hgs_i2(3)*(wk2(ix,iy,iz+ist)+wk2(ix,iy,iz-ist)) )
   end do
 end do
 end do
@@ -66,11 +70,15 @@ implicit none
 integer :: ix,iy,iz,ist
 complex(8) :: wk2(iwk2sta(1):iwk2end(1),iwk2sta(2):iwk2end(2),iwk2sta(3):iwk2end(3))
 complex(8) :: lap_wk(iwk3sta(1):iwk3end(1),iwk3sta(2):iwk3end(2),iwk3sta(3):iwk3end(3))
-real(8) :: f0
+real(8) :: f0, Hgs_i2(3)
 
 f0=(1.d0/Hgs(1)**2   &
    +1.d0/Hgs(2)**2   &
    +1.d0/Hgs(3)**2)
+
+Hgs_i2(1) = 1d0/Hgs(1)**2
+Hgs_i2(2) = 1d0/Hgs(2)**2
+Hgs_i2(3) = 1d0/Hgs(3)**2
 
 !$OMP parallel do private(ist)
 do iz=iwk3sta(3),iwk3end(3)
@@ -79,9 +87,9 @@ do ix=iwk3sta(1),iwk3end(1)
   lap_wk(ix,iy,iz)=cNmat(0,Nd)*f0*wk2(ix,iy,iz)
   do ist=1,Nd
     lap_wk(ix,iy,iz)=lap_wk(ix,iy,iz)     &
-           +cNmat(ist,Nd)*( wk2(ix+ist,iy,iz)/Hgs(1)**2 + wk2(ix-ist,iy,iz)/Hgs(1)**2       &
-                           +wk2(ix,iy+ist,iz)/Hgs(2)**2 + wk2(ix,iy-ist,iz)/Hgs(2)**2       &
-                           +wk2(ix,iy,iz+ist)/Hgs(3)**2 + wk2(ix,iy,iz-ist)/Hgs(3)**2  )
+           +cNmat(ist,Nd)*( Hgs_i2(1)*(wk2(ix+ist,iy,iz)+wk2(ix-ist,iy,iz))  &
+                           +Hgs_i2(2)*(wk2(ix,iy+ist,iz)+wk2(ix,iy-ist,iz))  &
+                           +Hgs_i2(3)*(wk2(ix,iy,iz+ist)+wk2(ix,iy,iz-ist)) )
   end do
 end do
 end do
