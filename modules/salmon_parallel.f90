d2692ddfa7f9479c53e6a82e7b2f3b5018148867
Yuta Hirokawa <hirokawa@hpcs.cs.tsukuba.ac.jp>
2017-06-08 10:21:28 +0900

    Add salmon_parallel module.

---
+++ modules/salmon_parallel.f90
@@ -0,0 +1,40 @@
+!
+!  Copyright 2017 SALMON developers
+!
+!  Licensed under the Apache License, Version 2.0 (the "License");
+!  you may not use this file except in compliance with the License.
+!  You may obtain a copy of the License at
+!
+!      http://www.apache.org/licenses/LICENSE-2.0
+!
+!  Unless required by applicable law or agreed to in writing, software
+!  distributed under the License is distributed on an "AS IS" BASIS,
+!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!  See the License for the specific language governing permissions and
+!  limitations under the License.
+!
+module salmon_parallel
+  implicit none
+
+  !!! multi process
+  ! global
+  integer, public :: nproc_group_global
+  integer, public :: nproc_id_global
+  integer, public :: nproc_size_global
+
+  !
+
+contains
+  function get_thread_id() result(nid)
+#ifdef _OPENMP
+    use omp_lib
+#endif
+    implicit none
+    integer :: nid
+#ifdef _OPENMP
+    nid = omp_get_thread_num()
+#else
+    nid = 0
+#endif
+  end function
+end module
