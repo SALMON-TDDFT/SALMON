0e68a4755ec40728b825897ddfc43cf12685ebc9
mn2007 <noda@ims.ac.jp>
2017-06-12 13:00:21 +0900

    remove a directory "scfrt-intel"

--- example/C2H2/scfrt-intel/gs.sh
+++
@@ -1,4 +0,0 @@
-export OMP_NUM_THREADS=24
-date > output_gs
-mpiexec -np 1 ../../../salmon.cpu < C2H2_gs.inp >> output_gs
-date >> output_gs
--- example/C2H2/scfrt-intel/rt_pulse.sh
+++
@@ -1,4 +0,0 @@
-export OMP_NUM_THREADS=24
-date > output_rt_pulse
-mpiexec -np 1 ../../../salmon.cpu < C2H2_rt_pulse.inp >> output_rt_pulse
-date >> output_rt_pulse
