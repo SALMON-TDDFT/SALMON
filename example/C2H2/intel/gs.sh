a0bab23c6e3ac9ab7592dc917bb4e2373cf26927
Yuta Hirokawa <hirokawa@hpcs.cs.tsukuba.ac.jp>
2017-08-09 16:28:15 +0900

    Modified examples directory.

--- example/C2H2/intel/gs.sh
+++
@@ -1,4 +0,0 @@
-export OMP_NUM_THREADS=24
-date > C2H2_gs.out
-mpiexec -np 1 ../../../salmon.cpu < C2H2_gs.inp >> C2H2_gs.out
-date >> C2H2_gs.out
--- example/C2H2/intel/rt_pulse.sh
+++
@@ -1,4 +0,0 @@
-export OMP_NUM_THREADS=24
-date > C2H2_rt_pulse.out
-mpiexec -np 1 ../../../salmon.cpu < C2H2_rt_pulse.inp >> C2H2_rt_pulse.out
-date >> C2H2_rt_pulse.out
