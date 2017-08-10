8f953f0392fcbc2734e50556a3188f5cbffb4e8a
Yuta Hirokawa <hirokawa@hpcs.cs.tsukuba.ac.jp>
2017-08-10 16:41:06 +0900

    Update version infomation.

--- cmake/get_git_info.cmake
+++ cmake/get_git_info.cmake
@@ -1,20 +1,34 @@
 # [Ref] CMake: Use Git Branch & Commit Details in Project
 #       http://xit0.org/2013/04/cmake-use-git-branch-and-commit-details-in-project/
 
-# Get the current working branch
-execute_process(
-  COMMAND git rev-parse --abbrev-ref HEAD
-  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_BRANCH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+find_package(Git)
 
-# Get the latest abbreviated commit hash of the working branch
-execute_process(
-  COMMAND git log -1 --format=%H
-  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_COMMIT_HASH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if (GIT_FOUND)
+  # Get the current working branch
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_BRANCH
+    RESULT_VARIABLE GIT_BRANCH_FOUND
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 
-message(STATUS "Git: ${GIT_COMMIT_HASH} in branch '${GIT_BRANCH}'")
+  # Get the latest abbreviated commit hash of the working branch
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} log -1 --format=%H
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_COMMIT_HASH
+    RESULT_VARIABLE GIT_COMMIT_HASH_FOUND
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  if (${GIT_BRANCH_FOUND} EQUAL 0)
+    message(STATUS "Git: ${GIT_COMMIT_HASH} in ${GIT_BRANCH}")
+  else ()
+    set(GIT_FOUND FALSE)
+  endif ()
+else ()
+  message(STATUS "Git not found.")
+endif ()
