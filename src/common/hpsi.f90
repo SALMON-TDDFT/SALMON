9d93fb81c34ff9e2af090352d75eaee7990fa07d
syamada0 <syamada@ccs.tsukuba.ac.jp>
2017-08-21 14:23:13 +0900

    test for new hpsi

--- src/common/hpsi.f90
+++ src/common/hpsi.f90
@@ -1,8 +1,18 @@
-
-! memo
-!       array: (ix,iy,iz,is,iorb,ik) --> (ix,iy,iz,iorb)
-!       pseudo: ARTED + comm_summation
-
+!
+!  Copyright 2017 SALMON developers
+!
+!  Licensed under the Apache License, Version 2.0 (the "License");
+!  you may not use this file except in compliance with the License.
+!  You may obtain a copy of the License at
+!
+!      http://www.apache.org/licenses/LICENSE-2.0
+!
+!  Unless required by applicable law or agreed to in writing, software
+!  distributed under the License is distributed on an "AS IS" BASIS,
+!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!  See the License for the specific language governing permissions and
+!  limitations under the License.
+!
 module hpsi_sub
 complex(8), parameter :: zI=(0.d0,1.d0)
 
