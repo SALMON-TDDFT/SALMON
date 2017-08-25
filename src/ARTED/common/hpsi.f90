c1811f121c767329f67a2f137f119d4ea1f27a9d
syamada0 <syamada@ccs.tsukuba.ac.jp>
2017-08-25 17:08:20 +0900

    option HPSI_TEST: joint hpsi subroutine

--- src/ARTED/common/hpsi.f90
+++ src/ARTED/common/hpsi.f90
@@ -44,8 +44,7 @@ contains
 
     tid = omp_get_thread_num()
     call init(tpsi,zhtpsi(:,1,tid))
-!    call hpsi_omp_KB_base(ik,zhtpsi(:,1,tid),zhtpsi(:,2,tid),zttpsi(:,tid))
-    call hpsi_test3(ik,zhtpsi(:,1,tid),zhtpsi(:,2,tid),zttpsi(:,tid))
+    call hpsi_omp_KB_base(ik,zhtpsi(:,1,tid),zhtpsi(:,2,tid),zttpsi(:,tid))
     call copyout(zhtpsi(:,2,tid),zttpsi(:,tid),htpsi,ttpsi)
 
    LOG_END(LOG_HPSI)
@@ -93,8 +92,7 @@ contains
     integer,intent(in)     :: ik
     complex(8),intent(in)  ::  tpsi(0:PNLz-1,0:PNLy-1,0:PNLx-1)
     complex(8),intent(out) :: htpsi(0:PNLz-1,0:PNLy-1,0:PNLx-1)
-!    call hpsi_omp_KB_base(ik,tpsi,htpsi)
-    call hpsi_test3(ik,tpsi,htpsi)
+    call hpsi_omp_KB_base(ik,tpsi,htpsi)
   end subroutine
 
   subroutine hpsi_omp_KB_base(ik,tpsi,htpsi,ttpsi)
@@ -182,109 +180,4 @@ contains
       end do
     end subroutine
   end subroutine
-
-!-----------------------------------------------------------------------------------------------------------------------------------
-  subroutine hpsi_test3(ik,tpsi,htpsi,ttpsi)
-    use hpsi_sub, only: hpsi_C
-    use timer
-    use Global_Variables, only: NLx,NLy,NLz,kAc,lapx,lapy,lapz,nabx,naby,nabz,Vloc,Mps,uV,iuV,Hxyz,ekr_omp,Nlma,a_tbl,Nps,NI,Jxyz
-    use opt_variables, only: lapt,PNLx,PNLy,PNLz,PNL
-#ifdef ARTED_USE_NVTX
-    use nvtx
-#endif
-    implicit none
-    integer,intent(in)              :: ik
-    complex(8),intent(in)           ::  tpsi(0:PNLz-1,0:PNLy-1,0:PNLx-1)
-    complex(8),intent(out)          :: htpsi(0:PNLz-1,0:PNLy-1,0:PNLx-1)
-    complex(8),intent(out),optional :: ttpsi(0:PNLz-1,0:PNLy-1,0:PNLx-1)
-    !
-    integer :: ipx_sta,ipx_end,ipy_sta,ipy_end,ipz_sta,ipz_end &
-              ,ix_sta,ix_end,iy_sta,iy_end,iz_sta,iz_end &
-              ,is_table(1),ik_table(1),i,irank_overlap(6),icomm_overlap,icomm_pseudo
-    real(8) :: lap0,lapt_wrk(4,3),nabt_wrk(4,3),kAc_wrk(1,3)
-    integer,allocatable :: idx(:),idy(:),idz(:),Jxyz_wrk(:,:,:)
-    real(8),allocatable :: uVu_wrk(:)
-
-    lap0 = -(lapx(0)+lapy(0)+lapz(0))*0.5d0
-
-    lapt_wrk(1:4,1) = lapz(1:4) ! x <--> z
-    lapt_wrk(1:4,2) = lapy(1:4)
-    lapt_wrk(1:4,3) = lapx(1:4) ! x <--> z
-
-    nabt_wrk(1:4,1) = nabz(1:4) ! x <--> z
-    nabt_wrk(1:4,2) = naby(1:4)
-    nabt_wrk(1:4,3) = nabx(1:4) ! x <--> z
-
-    kAc_wrk(1,1) = kAc(ik,3) ! x <--> z
-    kAc_wrk(1,2) = kAc(ik,2)
-    kAc_wrk(1,3) = kAc(ik,1) ! x <--> z
-
-    ix_sta = 0
-    ix_end = NLz-1 ! x <--> z
-    iy_sta = 0
-    iy_end = NLy-1
-    iz_sta = 0
-    iz_end = NLx-1 ! x <--> z
-
-    ipx_sta = 0
-    ipx_end = PNLz-1 ! x <--> z
-    ipy_sta = 0
-    ipy_end = PNLy-1
-    ipz_sta = 0
-    ipz_end = PNLx-1 ! x <--> z
-
-    is_table(1) = 1
-    ik_table(1) = 1
-
-    allocate(idx(ix_sta-4:ix_end+4),idy(iy_sta-4:iy_end+4),idz(iz_sta-4:iz_end+4))
-    do i=ix_sta-4,ix_end+4
-      idx(i) = mod(NLz+i,NLz) ! x <--> z
-    end do
-    do i=iy_sta-4,iy_end+4
-      idy(i) = mod(NLy+i,NLy)
-    end do
-    do i=iz_sta-4,iz_end+4
-      idz(i) = mod(NLx+i,NLx) ! x <--> z
-    end do
-
-    call convert_pseudo_ARTED(Jxyz_wrk,Jxyz,NI,Nps,NLy,NLz,Nlma,uVu_wrk,iuV,Hxyz)
-
-    call hpsi_C(tpsi,htpsi,ipx_sta,ipx_end,ipy_sta,ipy_end,ipz_sta,ipz_end,1 &
-           ,Vloc,ix_sta,ix_end,iy_sta,iy_end,iz_sta,iz_end,1 &
-           ,idx,idy,idz,lap0,lapt_wrk,is_table,1 &
-           ,NI,Nps,Nlma,a_tbl,Mps,Jxyz_wrk,uV,uVu_wrk &
-           ,1,irank_overlap,icomm_overlap,icomm_pseudo &
-           ,ik_table,nabt_wrk,kAc_wrk,ekr_omp(:,:,ik:ik),ttpsi)
-
-    deallocate(idx,idy,idz,Jxyz_wrk,uVu_wrk)
-
-    return
-  contains
-    subroutine convert_pseudo_ARTED(Jxyz_new,Jxyz,NI,Nps,NLy,NLz,Nlma,uVu,iuV,Hxyz)
-      integer :: NI,Nps,Jxyz(Nps,NI),NLy,NLz,Nlma,iuV(Nlma)
-      integer,allocatable :: Jxyz_new(:,:,:)
-      real(8) :: Hxyz
-      real(8),allocatable :: uVu(:)
-      !
-      integer    :: ia,j,i,ix,iy,iz ! i = ix*NLy*NLz + iy*NLz + iz + 1, ix=0:NLx-1, ...
-      allocate(Jxyz_new(3,Nps,NI),uVu(Nlma))
-      do ia=1,NI
-        do j=1,Nps
-          i = Jxyz(j,ia)
-          iz = mod(i-1,NLz)
-          iy = mod((i-1-iz)/NLz,NLy)
-          ix = (i-1-iz-iy*NLy)/(NLy*NLz)
-          Jxyz_new(1,j,ia) = iz ! x <--> z
-          Jxyz_new(2,j,ia) = iy
-          Jxyz_new(3,j,ia) = ix ! x <--> z
-        end do
-      end do
-      do i=1,Nlma
-        uVu(i) = dble(iuV(i)) * Hxyz
-      end do
-      return
-    end subroutine convert_pseudo_ARTED
-  end subroutine hpsi_test3
-!-----------------------------------------------------------------------------------------------------------------------------------
-
 end module
--- src/GCEED/rt/taylor.f90
+++ src/GCEED/rt/taylor.f90
@@ -78,184 +78,23 @@ if(ihpsieff==1)then
   end if
 end if
 
-!do nn=1,N_hamil
-!  if(ihpsieff==1)then
-!    if(mod(nn,2)==1)then
-!      call hpsi_groupob(tzpsi_in,htpsi,tzpsi_out,Vlocal2,nn,1)
-!    else
-!      call hpsi_groupob(htpsi,tzpsi_in,tzpsi_out,Vlocal2,nn,1)
-!    end if
-!  else
-!    if(mod(nn,2)==1)then
-!      call hpsi_groupob(tzpsi_in,htpsi,tzpsi_out,Vlocal,nn,1)
-!    else
-!      call hpsi_groupob(htpsi,tzpsi_in,tzpsi_out,Vlocal,nn,1)
-!    end if
-!  end if
-!end do
 
-!-----------------------------------------------------------------------------------------------------------------------------------
-do nn=1,N_hamil
-  if(mod(nn,2)==1) then
-    if(ihpsieff==1) then
-      call hpsi_test1(tzpsi_in,htpsi,Vlocal2)
-    else
-      call hpsi_test1(tzpsi_in,htpsi,Vlocal)
-    end if
-    call mode_add_polynomial(tzpsi_in,htpsi,tzpsi_out,nn)
-  else
-    if(ihpsieff==1) then
-      call hpsi_test1(htpsi,tzpsi_in,Vlocal2)
-    else
-      call hpsi_test1(htpsi,tzpsi_in,Vlocal)
-    end if
-    call mode_add_polynomial(htpsi,tzpsi_in,tzpsi_out,nn)
-  end if
-end do
-!-----------------------------------------------------------------------------------------------------------------------------------
-
-contains
-
-!-----------------------------------------------------------------------------------------------------------------------------------
-subroutine hpsi_test1(tpsi,htpsi,V)
-  use salmon_parallel, only: nproc_group_orbital
-  use hpsi_sub
-  use init_sendrecv_sub
-  use hpsi2_sub, only: convert_pseudo_GCEED
-  implicit none
-  complex(8) :: tpsi(mg_sta(1)-Nd:mg_end(1)+Nd+1,    &
-                     mg_sta(2)-Nd:mg_end(2)+Nd,    &
-                     mg_sta(3)-Nd:mg_end(3)+Nd,1:iobnum)
-  complex(8) :: htpsi(mg_sta(1)-Nd:mg_end(1)+Nd+1,    &
-                      mg_sta(2)-Nd:mg_end(2)+Nd,    &
-                      mg_sta(3)-Nd:mg_end(3)+Nd,1:iobnum)
-  real(8) :: V(mg_sta(1):mg_end(1),mg_sta(2):mg_end(2),mg_sta(3):mg_end(3),numspin)
-  !
-  integer :: ipx_sta,ipx_end,ipy_sta,ipy_end,ipz_sta,ipz_end &
-            ,ix_sta,ix_end,iy_sta,iy_end,iz_sta,iz_end &
-            ,ispin,i_all,Norb,i,iobmax,Nspin,Nk,ind,j,irank_overlap(6),icomm_pseudo,icomm_overlap,NI,Nps,Nlma
-  real(8) :: lap0,lapt(4,3)
-  integer, allocatable :: is_table(:),idx(:),idy(:),idz(:)
-
-  integer,allocatable :: ia_table(:),Mps(:),Jxyz_wrk(:,:,:)
-  real(8),allocatable :: uV_wrk(:,:),uVu_wrk(:)
-
-  irank_overlap(1) = iup_array(1)
-  irank_overlap(2) = idw_array(1)
-  irank_overlap(3) = jup_array(1)
-  irank_overlap(4) = jdw_array(1)
-  irank_overlap(5) = kup_array(1)
-  irank_overlap(6) = kdw_array(1)
-
-  lap0 = -0.5d0*cNmat(0,Nd)*(1.d0/Hgs(1)**2+1.d0/Hgs(2)**2+1.d0/Hgs(3)**2)
-  do j=1,3
-    do ind=1,4
-      lapt(ind,j) = cNmat(ind,Nd)/Hgs(j)**2
-    end do
-  end do
-
-  call calc_pmax(iobmax)
-  Norb = iobnum
-  Nspin = numspin
-  Nk = 1
-
-  ipx_sta = mg_sta(1)-Nd
-  ipx_end = mg_end(1)+Nd+1
-  ipy_sta = mg_sta(2)-Nd
-  ipy_end = mg_end(2)+Nd
-  ipz_sta = mg_sta(3)-Nd
-  ipz_end = mg_end(3)+Nd
-
-  ix_sta = mg_sta(1)
-  ix_end = mg_end(1)
-  iy_sta = mg_sta(2)
-  iy_end = mg_end(2)
-  iz_sta = mg_sta(3)
-  iz_end = mg_end(3)
-
-  allocate(is_table(Norb))
-  do i=1,iobmax
-    call calc_allob(i,i_all)
-    call set_ispin(i_all,ispin)
-    is_table(i) = ispin
-  end do
-
-  allocate(idx(ix_sta-4:ix_end+4),idy(iy_sta-4:iy_end+4),idz(iz_sta-4:iz_end+4))
-  do j=ix_sta-4,ix_end+4
-    idx(j) = j
-  end do
-  do j=iy_sta-4,iy_end+4
-    idy(j) = j
-  end do
-  do j=iz_sta-4,iz_end+4
-    idz(j) = j
-  end do
-
-  icomm_overlap = nproc_group_orbital
-  call convert_pseudo_GCEED(NI,Nps,Nlma,ia_table,Mps,uV_wrk,uVu_wrk,Jxyz_wrk,icomm_pseudo)
-
-  call hpsi_C(tpsi,htpsi,ipx_sta,ipx_end,ipy_sta,ipy_end,ipz_sta,ipz_end,Norb &
-                 ,V,ix_sta,ix_end,iy_sta,iy_end,iz_sta,iz_end,Nspin &
-                 ,idx,idy,idz,lap0,lapt,is_table,Nk &
-                 ,NI,Nps,Nlma,ia_table,Mps,Jxyz_wrk,uV_wrk,uVu_wrk &
-                 ,nproc_Mxin_mul,irank_overlap,icomm_overlap,icomm_pseudo)
-
-  deallocate(is_table,idx,idy,idz,Mps,Jxyz_wrk,uV_wrk,uVu_wrk,ia_table)
-  return
-end subroutine hpsi_test1
-
-! hpsi_groupob.f90 --> taylor.f90
-subroutine mode_add_polynomial(tpsi,htpsi,tpsi_out,nn)
-  implicit none
-  complex(8) :: tpsi(mg_sta(1)-Nd:mg_end(1)+Nd+1,    &
-                     mg_sta(2)-Nd:mg_end(2)+Nd,    &
-                     mg_sta(3)-Nd:mg_end(3)+Nd,1:iobnum,1)
-  complex(8) :: htpsi(mg_sta(1)-Nd:mg_end(1)+Nd+1,    &
-                      mg_sta(2)-Nd:mg_end(2)+Nd,    &
-                      mg_sta(3)-Nd:mg_end(3)+Nd,1:iobnum,1)
-  complex(8) :: tpsi_out(mg_sta(1)-Nd:mg_end(1)+Nd+1,    &
-                     mg_sta(2)-Nd:mg_end(2)+Nd,    &
-                     mg_sta(3)-Nd:mg_end(3)+Nd,1:iobnum,1)
-  integer :: nn
-  !
-  integer :: iobmax
-  call calc_pmax(iobmax)
-
-  if(N_hamil==1)then
-    if(ikind_eext==0)then
-      call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,5)
-    else
-      call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,4)
-    end if
-  else if(N_hamil==4)then
-    if(nn==1)then
-      if(ikind_eext==0)then
-        call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,1)
+  do nn=1,N_hamil
+    if(ihpsieff==1)then
+      if(mod(nn,2)==1)then
+        call hpsi_groupob(tzpsi_in,htpsi,tzpsi_out,Vlocal2,nn,1)
       else
-        call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,0)
+        call hpsi_groupob(htpsi,tzpsi_in,tzpsi_out,Vlocal2,nn,1)
       end if
-    else if(nn==3)then
-      call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,-1)
-    else if(nn==4)then
-      call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,6)
-    end if
-  else
-    if(nn==1)then
-      if(ikind_eext==0)then
-        call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,1)
-      else
-        call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,0)
-      end if
-    else if(nn==N_hamil)then
-      call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,6)
     else
-      call add_polynomial(tpsi,htpsi,tpsi_out,iobmax,nn,2)
+      if(mod(nn,2)==1)then
+        call hpsi_groupob(tzpsi_in,htpsi,tzpsi_out,Vlocal,nn,1)
+      else
+        call hpsi_groupob(htpsi,tzpsi_in,tzpsi_out,Vlocal,nn,1)
+      end if
     end if
-  end if
+  end do
 
-  return
-end subroutine
-!-----------------------------------------------------------------------------------------------------------------------------------
 
 end subroutine taylor
 
