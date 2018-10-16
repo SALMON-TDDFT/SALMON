b61a038674d66c13ed95e4ea8eca2beb700c5ecc
tt-ims <takeuchi@ims.ac.jp>
2018-10-16 13:50:31 +0900

    add classic em.

--- src/maxwell/classic_em.f90
+++ src/maxwell/classic_em.f90
@@ -15,13 +15,23 @@
 !
 !-----------------------------------------------------------------------------------------
 subroutine classic_em
-  use salmon_maxwell, only:fdtd_grid,fdtd_field,fdtd_tmp,init_maxwell,calc_maxwell,finalize_maxwell
+  use salmon_parallel,      only: nproc_id_global
+  use salmon_communication, only: comm_is_root
+  use salmon_maxwell,       only:fdtd_grid,fdtd_field,fdtd_tmp,init_maxwell,calc_maxwell,finalize_maxwell
+  use misc_routines,        only: get_wtime
   implicit none
   type(fdtd_grid)  :: grid
   type(fdtd_field) :: field
   type(fdtd_tmp)   :: tmp
+  real(8)          :: elapsed_time
   
+  elapsed_time=get_wtime()
   call init_maxwell(grid,field,tmp)
   call calc_maxwell(grid,field,tmp)
   call finalize_maxwell(grid,field,tmp)
+  elapsed_time=get_wtime()-elapsed_time
+  if(comm_is_root(nproc_id_global)) then
+    write(*,'(A,f16.8)') " elapsed time [s] = ", elapsed_time
+  end if
+  
 end subroutine classic_em
