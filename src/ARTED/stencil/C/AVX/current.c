8ff19e4f51147f505ae3b5df5f80076721fb507a
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-05-17 09:51:23 +0900

    doc: update copyright date

--- src/ARTED/stencil/C/AVX/current.c
+++ src/ARTED/stencil/C/AVX/current.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 SALMON developers
+ *  Copyright 2017-2019 SALMON developers
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
